dde62081e901568736e16270c51e475ee80f4b5b
dkorostashov-droid <d.korostashov@gmail.com>
2025-10-22 03:42:19 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -e
-# Створюємо каталоги (на випадок чистого диска)
 mkdir -p /app/docs /app/store
-# Запускаємо supervisord, який підніме і API, і бота
 exec supervisord -c /app/supervisord.conf
